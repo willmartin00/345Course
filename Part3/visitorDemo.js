d2c862065d1229f3e9799332ad4f0badadd4bc16
willmartin00 <87452212+willmartin00@users.noreply.github.com>
2022-03-27 23:15:57 -0400

    Update visitorDemo.js

--- Part3/visitorDemo.js
+++ Part3/visitorDemo.js
@@ -1,23 +1,82 @@
+var carVariable = function()
+{
+    var seats = 5;
+    var doors = 4;
+    this.accept = function(visitorObject)
+    {
+        visitorObject.visit(this);
+    }
+}
+var truckVariable = function()
+{
+    var towPackage = true;
+    var doors = 2;
+    this.accept = function(visitorObject)
+    {
+        visitorObject.visit(this);
+    }
+}
+var monsterTruckVariable = function()
+{
+    var looksLikeADragon = true;
+    var doors = 1.5;
+    this.accept = function(visitorObject)
+    {
+        visitorObject.visit(this);
+    }
+}
+
 var CarVisitor = function()
 {
-    var visit = function(carVariable)
+    this.visit = function(car)
     {
-         //do some operations on carVariable
+        if(car.seats > 2)
+        {
+            console.log('this is clearly a car for old people');
+        }
+        else
+        {
+            console.log('My bet is this car has at least 2 cylinders');
+        }
     }
 }
 
 var TruckVisitor = function()
 {
-    var visit = function(truckVariable)
+    this.visit = function(truckVar)
     {
-        //do some operations on truckVariable
+        if(truckVar.towPackage)
+        {
+            console.log('we need to buy a boat')
+        }
     }
 }
 
 var MonsterTruckVisitor = function()
 {
-    var visit = function(monsterTruckVariable)
+    this.visit = function(monsterTruckVar)
     {
-        //do some operations on monsterTruckVariable
+        if(monsterTruckVar.looksLikeADragon)
+        {
+            console.log('that is a badass monster truck')
+        }
+        else
+        {
+            console.log('loser')
+        }
     }
 }
+
+var myCar = new carVariable();
+myCar.seats = 2;
+myCar.accept(new CarVisitor());
+
+var myMonsterTruck = new monsterTruckVariable();
+myMonsterTruck.looksLikeADragon = false;
+myMonsterTruck.accept(new MonsterTruckVisitor());
+
+var myCar2 = new carVariable();
+myCar2.seats = 2;
+myCar2.accept(new MonsterTruckVisitor());
+
+
