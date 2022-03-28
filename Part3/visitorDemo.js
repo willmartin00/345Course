228be54a8457bf90224e418f6b5bb9c79f024871
willmartin00 <87452212+willmartin00@users.noreply.github.com>
2022-03-27 23:13:10 -0400

    Create visitorDemo.js

---
+++ Part3/visitorDemo.js
@@ -0,0 +1,23 @@
+var CarVisitor = function()
+{
+    var visit = function(carVariable)
+    {
+         //do some operations on carVariable
+    }
+}
+
+var TruckVisitor = function()
+{
+    var visit = function(truckVariable)
+    {
+        //do some operations on truckVariable
+    }
+}
+
+var MonsterTruckVisitor = function()
+{
+    var visit = function(monsterTruckVariable)
+    {
+        //do some operations on monsterTruckVariable
+    }
+}
